383e1bd627927b2bef015becc14e0b5af2c2baa8
Khurmatbek <xurmatbekolloyorov1514@gmail.com>
2023-09-19 09:08:27 +0500

    change bug

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const elbtn = document.querySelector(".header__btn");
 const dark = localStorage.getItem("mode");
-localStorage.setItem("mode","dark")
+localStorage.setItem("mode", "dark")
 if (localStorage.getItem("mode") == "dark") {
     darkMode();
 } else {
@@ -23,3 +23,87 @@ function noDark() {
     localStorage.setItem("mode", "light");
 
 }
+
+
+/*   
+ create part
+*/
+let regionArr = [];
+const list = document.querySelector(".flaggs__list");
+const form = document.querySelector(".form")
+const select = document.querySelector(".form__select");
+const fragment = new DocumentFragment();
+const fragmentselect = new DocumentFragment();
+
+
+
+function myrenderFunction(array) {
+    // list.innerHTML = ""
+    array.forEach(element => {
+        if (element.altSpellings[1] != undefined) {
+            const template = document.querySelector(".template_country").content;
+            const cloneTemplate = template.cloneNode(true);
+            cloneTemplate.querySelector(".flaggs__img").src = `${element.flags
+                .png}`;
+            cloneTemplate.querySelector(".flaggs__title").textContent = `${element.altSpellings[1]}`
+            cloneTemplate.querySelector(".size").textContent = `Populyation: ${element.population}`
+            cloneTemplate.querySelector(".region").textContent = `Region: ${element.region}`
+            cloneTemplate.querySelector(".capital").textContent = `Capital: ${element.capital}`
+            fragment.append(cloneTemplate);
+
+        }
+        list.append(fragment)
+        const findRegion = regionArr.find(item => {
+            return element.region == item
+        })
+        if (!findRegion) {
+            regionArr.push(element.region)
+        }
+
+
+
+    });
+    regionArr.forEach(item => {
+        // select.innerHTML=""
+        const template_select = document.querySelector(".template__select").content;
+        const secondClone = template_select.cloneNode(true);
+        secondClone.querySelector(".option").textContent = item;
+        secondClone.querySelector(".option").value = item;
+        fragmentselect.append(secondClone);
+        select.append(fragmentselect)
+    })
+
+
+
+}
+// const selectValue = select.value;
+
+async function MyGetFunction(url, render, region) {
+    try {
+        const respons = await fetch(url);
+        const data = await respons.json();
+        const result = data;
+        if (render) {
+            myrenderFunction(data.splice(0, 51), regionArr)
+        }
+        if (region) {
+            myrenderFunction(data, regionArr)
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+MyGetFunction("https://restcountries.com/v3.1/all", true, false)
+// list.innerHTML=""
+
+select.addEventListener("change", evt => {
+    // list.innerHTML=""
+    evt.preventDefault();
+    const selectValue = select.value;
+    console.log(selectValue)
+    MyGetFunction(`https://restcountries.com/v3.1/region/${selectValue}`, true, false);
+
+})
+
+
+
